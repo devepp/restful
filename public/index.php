c703575b6ec245ab52d554dfeda1b80c1900bf83
Matt <MF-Billings@users.noreply.github.com>
2019-07-31 10:51:46 -0400

    test container

--- public/index.php
+++ public/index.php
@@ -1,7 +1,20 @@
 <?php
 
-use App\Router;
+use App\Core\Container\Alpha;
+use App\Core\Container\Container;
+use Psr\Container\ContainerInterface;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-echo 'You are a success!';
+// echo 'You are a success!';
+
+$entries = [
+    Alpha::class => function () {
+        return new Alpha();
+    }
+];
+
+$c = new Container($entries);
+
+$alpha = $c->get(Alpha::class);
+echo $alpha->name;
