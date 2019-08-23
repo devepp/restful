d5ac9ddd692e38feb2b023db1db76e4889cc273b
Paul Epp <Paul.Epp@ebasefm.com>
2019-08-23 14:29:20 -0400

    reorganize container factories

--- src/Core/Container/ContainerBuilder.php
+++ src/Core/Container/ContainerBuilder.php
@@ -2,11 +2,22 @@
 
 namespace App\Core\Container;
 
+use Psr\Container\ContainerInterface;
+use Zend\Diactoros\ResponseFactory;
+use App\Core\Router\RouteCollector;
+use App\Core\Router\RouteCollection;
+use App\Core\Router\RouterInterface;
+use App\Core\Router\Router;
+use App\Controllers\AssetsController;
+use App\Core\PDOManager;
+use App\Core\Middleware\MiddlewareCollection;
+use App\Core\Middleware\MiddlewareCollector;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 
 class ContainerBuilder
 {
-    private $coreFactories = [];
-
     public function build()
     {
         return new Container($this->getMergedFactories());
@@ -16,6 +27,37 @@ class ContainerBuilder
     {
         $factories = [];
         require_once('../config/container_factories.php');
-        return array_merge($this->coreFactories, $factories);
+        return array_merge($this->getCoreFactories(), $factories);
+    }
+
+    private function getCoreFactories()
+    {
+        return [
+            ResponseFactoryInterface::class => function(ContainerInterface $c) {
+                return new ResponseFactory();
+            },
+            ContainerInterface::class => function(ContainerInterface $c) {
+                return $c;
+            },
+            RouterInterface::class => function(ContainerInterface $c) {
+                return new Router($c->get(RouteCollection::class));
+            },
+            RouteCollection::class => function(ContainerInterface $c) {
+                $routeCollector = $c->get(RouteCollector::class);
+                require_once('../config/routes.php');
+                return $routeCollector->getCollection();
+            },
+            ServerRequestInterface::class => function(ContainerInterface $c) {
+                return new AssetsController($c->get(\PDO::class));
+            },
+            MiddlewareCollection::class => function(ContainerInterface $c) {
+                $middlewares = $c->get(MiddlewareCollector::class);
+                require_once('../config/middlewares.php');
+                return $middlewares->collection();
+            },
+            PDO::class => function(ContainerInterface $c) {
+                return PDOManager::getInstance()->getConnection();
+            }
+        ];
     }
 }
