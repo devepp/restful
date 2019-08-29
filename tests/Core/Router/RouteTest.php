9f7a1048432314be664c00208ab753e050d5e99d
Will Thiessen <Will.Thiessen@dms-technologies.com>
2019-08-29 10:56:57 -0400

    Implement getParameters to RouteTest

--- tests/Core/Router/RouteTest.php
+++ tests/Core/Router/RouteTest.php
@@ -2,10 +2,32 @@
 
 namespace App\Core;
 
-
+use App\Core\Router\Route;
 use PHPUnit\Framework\TestCase;
 
 class RouteTest extends TestCase
 {
+   public function testReturnsParameters()
+   {
+      $route = new Route('GET', '/assets/{id}', 'AssetsController', 'index');
 
+      $parameters = $route->getParameters('/assets/5');
+
+      $expectReturnedArray = [
+         'id' => '5'
+      ];
+
+      $this->assertEquals($parameters, $expectReturnedArray);
+   }
+
+   public function testDoesNotReturnsParameters()
+   {
+      $route = new Route('GET', '/assets/example', 'AssetsController', 'index');
+
+      $parameters = $route->getParameters('/assets/5');
+
+      $expectReturnedArray = [];
+
+      $this->assertEquals($parameters, $expectReturnedArray);
+   }
 }
