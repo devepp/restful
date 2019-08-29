205b70f34d053afcfbcfee8af7ab65f1e33f36e4
Michael MacDonald <Michael.MacDonald@dms-technologies.com>
2019-08-29 10:54:24 -0400

    add Route->methodAllowed tests

--- tests/Core/Router/RouteTest.php
+++ tests/Core/Router/RouteTest.php
@@ -2,10 +2,20 @@
 
 namespace App\Core;
 
-
+use App\Core\Router\Route;
 use PHPUnit\Framework\TestCase;
 
 class RouteTest extends TestCase
 {
+	public function testMethodAllowed()
+	{
+		$route = new Route('GET', '', '', '');
+		$this->assertTrue($route->methodAllowed('GET'));
+	}
 
+	public function testMethodNotAllowed()
+	{
+		$route = new Route('GET', '', '', '');
+		$this->assertFalse($route->methodAllowed('POST'));
+	}
 }
